a8d9729a33e20bdbce924d0e855d8b0350687b8d
Chukas <lukashuerfano@yahoo.com>
2024-09-29 17:37:47 -0500

    Cambios finales ya con el index arregaldo(Funciona el area)
    Funcion correctamente del area,falta probar los animales

--- src/routes/area.js
+++ src/routes/area.js
@@ -11,26 +11,26 @@ router.post("/areas", (req, res) => {
         }).catch((error) => res.send(error));
 })
 
-router.put("/areas/:id",async(req,res)=>{
-    const{ id }=req.params;
-    const animal=animalSchema(req.body);
-    var idAnimal=null;
+router.put("/areas/:id", async (req, res) => {
+    const { id } = req.params;
+    const animal = animalSchema(req.body);
+    var idAnimal = null;
 
-    const animalConsulta= await animalSchema.findOne({codigo: req.body.codigo});
-    if(!animalConsulta){
-        await animal.save().then((dataAnimal)=>{
-            idAnimal=dataAnimal._id;
+    const animalConsulta = await animalSchema.findOne({ codigo: req.body.codigo });
+    if (!animalConsulta) {
+        await animal.save().then((dataAnimal) => {
+            idAnimal = dataAnimal._id;
         });
-    }else{
-        idAnimal=animalConsulta._id;
+    } else {
+        idAnimal = animalConsulta._id;
     }
 
     areaSchema
-    .updateOne({_id:id},{
-        $addToSet:{animales:idAnimal}
-    })
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+        .updateOne({ _id: id }, {
+            $addToSet: { animales: idAnimal }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
 
-module.exports=router;
+module.exports = router;
